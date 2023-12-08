e010929580ec6df59c2fff0921e7d90c998bd2fc
IvanOskolkov <oskolkov427@gmail.com>
2023-12-08 21:21:52 +0700

    practica

---
+++ practica 1.3/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFilePath = "nums.txt";
+        
+        string outputFilePath = "filtered_nums.txt";
+        
+        try
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (string line in lines)
+                {
+                    string[] numbers = line.Split(' ');
+                    
+                    foreach (string number in numbers)
+                    {
+                        if (int.TryParse(number, out int num))
+                        {
+                            if (num % 2 != 0)
+                            {
+                                writer.Write(num + " ");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Ошибка при преобразовании строки в число: {number}");
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
+            Console.WriteLine("Четные числа удалены. Результат сохранен в файле: " + outputFilePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+    }
+}
---
+++ practica 1.3/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите высоты столбцов через пробел:");
+        string input = Console.ReadLine();
+        
+        string[] heightStrings = input.Split(' ');
+        
+        int[] height = new int[heightStrings.Length];
+        
+        for (int i = 0; i < heightStrings.Length; i++)
+        {
+            if (int.TryParse(heightStrings[i], out int h))
+            {
+                height[i] = h;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка при преобразовании строки в число.");
+                return;
+            }
+        }
+        
+        int maxArea = MaxArea(height);
+        Console.WriteLine($"Наибольшая площадь контейнера: {maxArea}");
+    }
+
+    static int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        int left = 0;    
+        int right = height.Length - 1;   
+
+        while (left < right)
+        {
+            int width = right - left;
+            
+            int h = Math.Min(height[left], height[right]);
+            
+            int area = width * h;
+            
+            maxArea = Math.Max(maxArea, area);
+            
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
